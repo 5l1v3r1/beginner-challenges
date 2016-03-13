87510b51ecbf13c548e8c685cea01c4f4ff13328
Alex Nichol <unixpickle@gmail.com>
2016-03-13 16:25:34 -0400

    figure out real:complex root ratio in random polynomials

--- solutions/polynomials/main.go
+++ solutions/polynomials/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	parts := []string{"x - 3", "x - 2", "x - 1", "x", "x + 1", "x + 2", "x + 3"}
@@ -10,5 +14,20 @@ func main() {
 		res = res.Multiply(p)
 	}
 	factorMe := res.Add(Polynomial{1})
-	fmt.Println("Factors of", factorMe, "are", factorMe.Roots())
+	fmt.Println("Roots of", factorMe, "are", factorMe.Roots())
+
+	// Figure out the % of roots of 8th degree polynomials that are real.
+	rand.Seed(time.Now().UnixNano())
+	totalRootCount := 0
+	totalPossibleRootCount := 0
+	for i := 0; i < 100000; i++ {
+		randPoly := make(Polynomial, 9)
+		for i := range randPoly {
+			randPoly[i] = rand.Float64()*2 - 1
+		}
+		totalRootCount += len(randPoly.Roots())
+		totalPossibleRootCount += len(randPoly) - 1
+	}
+	fmt.Println("Polynomials had", 100*float64(totalRootCount)/float64(totalPossibleRootCount),
+		"percent real roots.")
 }
